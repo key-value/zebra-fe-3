44b5946c9dbdb22f185fa13e6f6ddb1b665d398b
sixteen <yinyazuolun@gmail.com>
2019-06-27 23:06:54 +0800

    add axios  error

--- zebra-fe/src/main.ts
+++ zebra-fe/src/main.ts
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui';
+import axios, { AxiosInstance } from 'axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
+declare module 'Vue/types/vue' {
+  interface Vue {
+    $axios: AxiosInstance
+  }
+}
 
 Vue.use(ElementUI)
 
--- zebra-fe/src/pages/plat/schedule.vue
+++ zebra-fe/src/pages/plat/schedule.vue
@@ -3,3 +3,11 @@
     <h1>This is an Schedule page</h1>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  mounted(){
+    
+  }
+})
+</script>
