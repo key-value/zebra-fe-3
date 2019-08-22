57f99c408807963e599e904777ba005ba793e9dd
sixteen <yinyazuolun@sina.com>
2019-08-22 22:25:26 +0800

    task sort api

--- zabra-be/src/plat/task/task.controller.ts
+++ zabra-be/src/plat/task/task.controller.ts
@@ -7,35 +7,35 @@ import Task from './task.model';
 @ApiUseTags('task')
 @Controller('task')
 export class TaskController {
-    constructor(
-      private readonly taskService: TaskService,
-    ) {}
+  constructor(
+    private readonly taskService: TaskService,
+  ) { }
 
-    @Get(`:id`)
-    async getOne(@Param('id')id: number): Promise<Task> {
-      return await this.taskService.get(id)
-    }
-    @Get()
-    async getAll(): Promise<Task[]> {
-      return await this.taskService.getAll()
-    }
-
-    @Post()
-    async add(@Body() body: CreateTaskDto) {
-      await this.taskService.add(body.taskTitle, body.planId, body.description)
-    }
-
-    @Put(':id/planId/{planId}/sort/{num}')
-    async updateSort(@Param('id') id: number, @Param('planId') planId:number , @Param('num') num:number ) {
-      await this.taskService.updateSort(id,planId, num)
-    }
-
-    @Put(':id')
-    async update(@Param('id') id: number, @Body() body: CreateTaskDto) {
-      await this.taskService.update(id, body.taskTitle, body.planId, body.description)
-    }
-    @Delete(':id')
-    async delete(@Param('id') id: number){
-      await this.taskService.delete(id)
-    }
+  @Get(`:id`)
+  async getOne(@Param('id') id: number): Promise<Task> {
+    return await this.taskService.get(id)
   }
+  @Get()
+  async getAll(): Promise<Task[]> {
+    return await this.taskService.getAll()
+  }
+
+  @Post()
+  async add(@Body() body: CreateTaskDto) {
+    await this.taskService.add(body.taskTitle, body.planId, body.description)
+  }
+
+  @Put(':id/planId/:planId/sort/:num')
+  async updateSort(@Param('id') id: number, @Param('planId') planId: number, @Param('num') num: number) {
+    await this.taskService.updateSort(id, num, planId)
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() body: CreateTaskDto) {
+    await this.taskService.update(id, body.taskTitle, body.planId, body.description)
+  }
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    await this.taskService.delete(id)
+  }
+}
